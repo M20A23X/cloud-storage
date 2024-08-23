79593e80b3e7e00b2a4985138c73551b8ad9b2cd
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2024-08-23 20:54:10 +0000

    Feat(client): add file upload

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {createDir, getFiles} from "../../actions/file";
+import {createDir, getFiles, uploadFile} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.css'
 import Popup from "./Popup";
@@ -18,11 +18,20 @@ const Disk = () => {
         dispatch(setPopupDisplay('flex'))
     }
 
+    function fileUploadHandler(event) {
+        const files = [...event.target.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns">
                 <button className="disk__back">Back</button>
                 <button className="disk__create" onClick={() => showPopupHandler()}>Create dir</button>
+                <div className="disk__upload">
+                    <label htmlFor="disk__upload-input" className="disk__upload-label">Upload file</label>
+                    <input multiple={true} onChange={(event)=> fileUploadHandler(event)} type="file" id="disk__upload-input" className="disk__upload-input"/>
+                </div>
             </div>
             <FileList/>
             <Popup/>
